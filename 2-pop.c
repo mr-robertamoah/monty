3f4e709951f572fe4827e5aed16c13992a2fa86a
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-25 22:27:54 +0100

    made corrections to pop function

--- 2-pop.c
+++ 2-pop.c
@@ -11,7 +11,7 @@ void pop(stack_t **stack, unsigned int line_number)
 	(void) stack;
 	if (argument->stackHead == NULL)
 	{
-		fprintf(stderr, "L%d: can't pop, stack empty\n", line_number);
+		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
 		closeStream();
 		free_toks();
 		free_arg();
