a2dcba1515fdf756b8318a5f2c9396071129071b
Robert Amoah <mr_robertamoah@yahoo.com>
2023-05-25 23:01:58 +0100

    implemented pchar and pstr instructions

--- 12-pstr.c
+++ 12-pstr.c
@@ -9,28 +9,20 @@
 
 void pstr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp1, *tmp2;
+	stack_t *tmp1;
 
 	(void) stack;
-	if (argument->stack_length < 2)
-	{
-		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
-		free_all_args();
-		exit(EXIT_FAILURE);
-	}
+	(void) line_number;
 
 	tmp1 = argument->stackHead;
-	tmp2 = tmp1->next;
 
-	if (tmp1->n == 0)
+	while (tmp1 != NULL)
 	{
-		fprintf(stderr, "L%d: division by zero\n", line_number);
-		free_all_args();
-		exit(EXIT_FAILURE);
+		if (tmp1->n <= 0 || tmp1->n > 127)
+			break;
+		printf("%c", tmp1->n);
+		tmp1 = tmp1->next;
 	}
 
-	tmp2->n = tmp2->n % tmp1->n;
-	delete_stack_node();
-
-	argument->stack_length -= 1;
+	printf("\n");
 }
