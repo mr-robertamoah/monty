4b21f43f2033a939db6e73e5445336fa52fe8904
Robert Amoah <mr_robertamoah@yahoo.com>
2023-06-17 07:58:16 +0100

    made corrections to 14-rotr.c

--- 14-rotr.c
+++ 14-rotr.c
@@ -8,22 +8,27 @@
 
 void rotr(stack_t **stack, unsigned int line_number)
 {
-	stack_t *tmp1, *tmp2;
+	stack_t *tmp, *last;
 
 	(void) stack;
 	(void) line_number;
 	if (argument->stack_length < 2)
 		return;
 
-	tmp1 = tmp2 = argument->stackHead;
-	while (tmp2->next != NULL)
-		tmp2 = tmp2->next;
+	tmp = argument->stackHead;
+	while (tmp)
+	{
+		if (tmp->next == NULL)
+		{
+			last = tmp;
+			break;
+		}
+		tmp = tmp->next;
+	}
 
-	if (tmp2->prev)
-		tmp2->prev->next = NULL;
-	tmp2->prev = NULL;
-	tmp2->next = tmp1;
-	tmp1->prev = tmp2;
+	last->prev->next = NULL;
+	last->next = argument->stackHead;
+	last->prev = NULL;
 
-	argument->stackHead = tmp2;
+	argument->stackHead = last;
 }
